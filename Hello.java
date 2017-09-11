3d129d0604c55031903b1688d381790f5c690d95
周文韬 <wentao@dhcp-10-134-204-182.uawifi.arizona.edu>
2017-09-11 16:46:33 -0700

    se

--- Hello.java
+++ Hello.java
@@ -9,28 +9,30 @@ public class Hello {
     }
 
     public static void callSign() {
-        
+
         public static voide main(String[] args) {
             System.out.println("Lock S-foils in attack position.")
+            System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.")
 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
 
     public static void goldLeader() {
-
+        public static voide main(String[] args) {
+            System.out.println("Loosen up!")
         // Gold Leader should add a print statement that says "Loosen up!"
-    
+
 
     }
 
     public static void redLeader() {
- 
+
         // Red Leader should add a print statement that says "I copy, gold leader!"
         public static voide main(String[] args) {
             System.out.println("I copy, gold leader!")
         }
-        
+
 
     }
 
