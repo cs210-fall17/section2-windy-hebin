ecd9fed2369f597abfba590a50ef2f8f2012e707
Ye Bin Brandt <yebinbrandt@akita.cs.arizona.edu>
2017-09-11 16:59:44 -0700

    redleader

--- Hello.java
+++ Hello.java
@@ -10,28 +10,25 @@ public class Hello {
 
     public static void callSign() {
 
-        public static void main(String[] args) {
-            System.out.println("Lock S-foils in attack position.");
-            System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
+        System.out.println("Lock S-foils in attack position.");
+        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-        }
+        
     }
 
     public static void goldLeader() {
-        public static void main(String[] args) {
-            System.out.println("Loosen up!");
+        System.out.println("Loosen up!");
         // Gold Leader should add a print statement that says "Loosen up!"
-        }
+        
 
     }
 
     public static void redLeader() {
 
         // Red Leader should add a print statement that says "I copy, gold leader!"
-        public static void main(String[] args); {
-            System.out.println("I copy, gold leader!");
-        }
+        System.out.println("I copy, gold leader!");
+        
 
 
     }
